e62790d7e31070f94aa355af3c280a29c1f94973
shakilla1 <s.uwamahoro1@alustudent.com>
2026-01-10 15:07:08 -0800

    add change

--- models/base_model.py
+++ models/base_model.py
@@ -3,33 +3,37 @@ import uuid
 from datetime import datetime
 import models
 
+
 class BaseModel:
     """ this is the parent class that handles id create timestamp"""
 
     def __init__(self, *args, **kwargs):
         """initalize the base model"""
-
         if kwargs:
             for key, value in kwargs.items():
-             if key != "__class__":
-                if key == "created_at" or key == "updated_at":
-                   value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
-                setattr(self, key, value)
+                if key != "__class__":
+                    if key == "created_at" or key == "updated_at":
+                        value = datetime.strptime(
+                            value, "%Y-%m-%dT%H:%M:%S.%f"
+                        )
+                    setattr(self, key, value)
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
             models.storage.new(self)
 
-        
     def __str__(self):
         """returns the string representation of the BaseModel"""
-        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
-    
+        return "[{}] ({}) {}".format(
+            self.__class__.__name__,
+            self.id,
+            self.__dict__
+        )
+
     def save(self):
-       
-       self.updated_at = datetime.now()
-       models.storage.save()
+        self.updated_at = datetime.now()
+        models.storage.save()
 
     def to_dict(self):
         """ returns a dictionary containing all keys/values of __dict__ of the instance"""
@@ -37,8 +41,7 @@ class BaseModel:
         if "created_at" in new_dict:
             new_dict["created_at"] = self.created_at.isoformat()
         if "updated_at" in new_dict:
-            new_dict["updated_at"] = self.updated_at.isoformat()    
+            new_dict["updated_at"] = self.updated_at.isoformat()
         new_dict["__class__"] = self.__class__.__name__
         return new_dict
-    
     
--- models/place.py
+++ models/place.py
@@ -1,23 +1,18 @@
-#!/user/bin/python3
+#!/usr/bin/python3
 """place module for the AirBnB clone"""
 
 from models.base_model import BaseModel
 
 
 class Place(BaseModel):
-
-    # class attributes
-    city_id = ""  
-    user_id = ""  
-    name = ""  
-    description = ""  
-    number_rooms = 0  
+    city_id = ""
+    user_id = ""
+    name = ""
+    description = ""
+    number_rooms = 0
     number_bathrooms = 0
-    max_guest = 0  
-    price_by_night = 0  
+    max_guest = 0
+    price_by_night = 0
     latitude = 0.0
-    longitude = 0.0 
-    amenity_ids = []  
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+    longitude = 0.0
+    amenity_ids = []
